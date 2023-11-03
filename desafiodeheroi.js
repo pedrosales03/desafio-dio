9877e6e6d44e4f6406267b040d6ae71fc3f8e963
pedrosales03 <146766495+pedrosales03@users.noreply.github.com>
2023-11-03 11:35:18 -0300

    Add files via upload

---
+++ desafiodeheroi.js
@@ -0,0 +1,40 @@
+let nome = "Pantera negra"
+let xp = 1000
+let nivel;
+
+switch (xp < 1000) {
+case xp < 1000:
+nivel = "Ferro";
+break;
+
+case xp >= 1001 && xp <= 2000:
+nivel = "Bronze"
+break;
+
+
+case xp >= 6001 && xp <= 5000:
+nivel = "Prata"
+break;
+
+case xp >= 6001 && xp <= 7000:
+nivel = "Ouro"
+break;
+
+case xp >= 6001 && xp <= 7000:
+nivel = "Platina"
+break;
+
+case xp >= 6001 && xp <= 7000:
+nivel = "Ascendente"
+break;
+
+case xp >= 6001 && xp <= 7000:
+nivel = "Imortal"
+break;
+
+case xp >= 6001 && xp <= 7000:
+nivel = "Radiante"
+break;
+}
+
+console.log("O Herói de nome" + nome + " esta no nivel de " + nivel);
